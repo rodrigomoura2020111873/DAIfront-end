ced618930d342ef84c104cd0d00e27cd1cc39089
Resendesfr <117100339+Resendesfr@users.noreply.github.com>
2023-07-03 15:14:32 +0000

    Markers dos baldes
    Adicionado um marker para cada balde, que ao clicar no marker mostra as informações dos baldes

--- src/pages/Mapa.js
+++ src/pages/Mapa.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import MainNavBar from '../components/NavBar/MainNavBar';
 
 
@@ -9,14 +9,15 @@ const containerStyle = {
 };
 
 const center = {
-  lat: 37.74611218769027,
-  lng: -25.662894527435547
+  lat: 0, //37.74611218769027,
+  lng: 0 //-25.662894527435547
 };
 
 const Mapa = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [data, setData] = useState([]);
+    const [selectedMarker, setSelectedMarker] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -33,6 +34,14 @@ const Mapa = () => {
     
         fetchData();
       }, []);
+
+      const handleMarkerClick = (marker) => {
+        setSelectedMarker(marker);
+      };
+    
+      const handleCloseInfoWindow = () => {
+        setSelectedMarker(null);
+      };
     
     
   return (
@@ -40,6 +49,27 @@ const Mapa = () => {
     <MainNavBar />
     <LoadScript googleMapsApiKey="AIzaSyAtPh6llWn2OSoWEDmS7oiTmtV5eDMLEiE">
     <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
+      {data.map((balde) => (
+            <Marker 
+            key={balde.id} 
+            position={{ lat: balde.localizacao[0], lng: balde.localizacao[1] }}
+            onClick={() => handleMarkerClick(balde)}
+             />
+          ))}
+      {selectedMarker && (
+        <InfoWindow
+          position={{ lat: selectedMarker.localizacao[0], lng: selectedMarker.localizacao[1] }}
+          onCloseClick={handleCloseInfoWindow}
+        >
+          <div>
+            <h6>{selectedMarker.conselho}</h6>
+            <h6>{selectedMarker.freguesia}</h6>
+            <p>Latitude: {selectedMarker.localizacao[0]} Longitude: {selectedMarker.localizacao[1]}</p>
+            <p>Tipo: {selectedMarker.tipo}</p>
+            <p>Capacidade: {selectedMarker.percentagem_lixo} % </p>
+          </div>
+        </InfoWindow>
+      )}
         </GoogleMap>
     </LoadScript>
     </>
